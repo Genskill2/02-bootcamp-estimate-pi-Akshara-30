c6f487ed9a0a9dbb327e06135d3674bd9e483e43
akshara <akshara.anil2000@gmail.com>
2021-06-01 22:47:33 +0530

    new changes

--- estimate.py
+++ estimate.py
@@ -1,28 +1,27 @@
 import math
 import unittest
 import random
-
 class TestWallis(unittest.TestCase):
-
     def wallis(self, n):
         pi_2 = 4/3
-        for n in range(2,n+1):
-            pi_2 *= (4*(n**2))/(4*(n**2) - 1)
+        for i in range(2,n+1):
+            pi_2 *= (4*(i**2))/(4*(i**2) - 1)
         return 2*pi_2
+    
     def test_low_iters(self):
         for i in range(0, 5):
-            pi = wallis(i)
+            pi = self.wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
             
     def test_high_iters(self):
         for i in range(500, 600):
-            pi = wallis(i)
+            pi = self.wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+    
 
 
 class TestMC(unittest.TestCase):
-
- def monte_carlo(self,n):
+    def monte_carlo(self,n):
         count = 0
         for i in range(n):
             randx = random.random()
@@ -32,10 +31,10 @@ class TestMC(unittest.TestCase):
                 count+=1
     
         return 4*(count/n)
-
+    
     def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
+        pi0 = self.monte_carlo(15000)
+        pi1 = self.monte_carlo(15000)
         
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
@@ -43,7 +42,7 @@ class TestMC(unittest.TestCase):
 
     def test_accuracy(self):
         for i in range(500, 600):
-            pi = monte_carlo(i)
+            pi = self.monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
     
